e8e1e3b36100c3cfb9d01e571dd78a91a2098c4c
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-11-24 11:45:45 -0300

    initial commit

---
+++ src/main/java/br/com/carrinhoCompras/CarrinhoDeComprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.carrinhoCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carrinhoCompras/CarrinhoDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carrinhoCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
